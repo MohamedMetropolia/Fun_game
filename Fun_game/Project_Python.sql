354e1f240f5edd67cda7c6fe8c07597ba9d8a443
Jeferson <jefersod@metropolia.fi>
2022-12-06 17:43:49 +0200

    Updated DB with directions, items and possible names for locations PNG

--- Fun_game/Project_Python.sql
+++ Fun_game/Project_Python.sql
@@ -71,34 +71,39 @@ create table visited_locations
 # Adding the data to the tables
 insert into items(description)
 values ("Nothing"),
-       ("feather"),
+       ("Feather"),
        ("Gold"),
-       ("Sword");
+       ("Sword"),
+       ("Food Offerings"),
+       ("Invisible Cloak"),
+       ("Holy Chalice"),
+       ("Shield"),
+       ("Gemstone");
 
 insert into location(description, location_text, map_image, encounter, encounter_text, encounter_image, use_item_decision, encounter_win_url, encounter_win_text, encounter_loose_url, encounter_loose_text, item, item_id, item_image, item_description, item_decision_text, direction_up, direction_down, direction_left, direction_right)
 values ("Maze","You find yourself waking up in a maze. You have to find the exit in order to survive.","maze_map.png",0,"","","","","","","",0,0,"","","",3,0,4,2),
-       ("Basement","You are walking downstairs to a cold and damp place with little to no light. It seems to be an old basement.","basement_map.png",0,"","","","","","","",1,1,"sword.png","Sword","Under a dusty blanket there seems to be a Sword. It might come in handy later on... Would you take it?",11,0,1,16),
-       ("Crossroad","As you were moving you realize you''ve come to an intersection in the maze","",0,"","","","","","","",0,0,"","","",0,0,0,0),
-       ("Graveyard","In the distance you see some slim silhouettes in the fog. As you walk closer, you realize that those are tombstones and you find yourself in the middle of a graveyard.","",0,"","","","","","","",0,0,"","","",0,0,0,0),
-       ("Forest","You find yourself entering a forest","",0,"","","","","","","",0,0,"","","",0,0,0,0),
-       ("Necropolis","You enter an extensive and elaborate burial place of an ancient city","",0,"","","","","","","",0,0,"","","",0,0,0,0),
-       ("Fork","This path leads you outside to a fork","",0,"","","","","","","",0,0,"","","",0,0,0,0),
-       ("Treasure Room","You find yourself walking into a treasure room!","",0,"","","","","","","",0,0,"","","",0,0,0,0),
-       ("Cemetery","You enter the cemetery causing the undead to become restless","",0,"","","","","","","",0,0,"","","",0,0,0,0),
-       ("Shed","You find yourself walking in a shed full of feathers!","",0,"","","","","","","",0,0,"","","",0,0,0,0),
-       ("Bandit Camp","You find yourself in the middle of a bandit camp, they don't look friendly","",0,"","","","","","","",0,0,"","","",0,0,0,0),
-       ("Pond","You hear a splashing and see a pond up ahead, a mermaid greets you from the water","",0,"","","","","","","",0,0,"","","",0,0,0,0),
-       ("Inn","The smell of food and the sound of laughter greets you as you find yourself at the inn.","",0,"","","","","","","",0,0,"","","",0,0,0,0),
-       ("Shrine","A holy glow radiates from between the trees. A stone shrine is sitting in a clearing in the woods, surrounded by offerings.","",0,"","","","","","","",0,0,"","","",0,0,0,0),
-       ("Scholar's Lair","A twirling tower looms above you. You see books, tomes and scrolls inside, and a strong arcane energy fills the air","",0,"","","","","","","",0,0,"","","",0,0,0,0),
-       ("Hut","A worn-down hut stands near the entrance to the forest. An old man is sitting on the porch, carving a wooden figure. From the looks of it, he is making a wooden wolf.","",0,"","","","","","","",0,0,"","","",0,0,0,0),
-       ("Chapel","A sense of dread sits in your stomach as you approach the cold, stone walls of a chapel. The air inside is damp, and the very ground beneath your feet seems to hold its breath.","",0,"","","","","","","",0,0,"","","",0,0,0,0),
-       ("Cave","You approach an opening in the mountain which is covered in stalactites. It looks like a beast, baring its teeth at you.","",0,"","","","","","","",0,0,"","","",0,0,0,0),
-       ("Troll Hideout","A putrid smell hits your nose as you find yourself face-to-face with three hulking figures - trolls!","",0,"","","","","","","",0,0,"","","",0,0,0,0),
-       ("Marsh","Your feet stick to the ground as you make your way through a marsh. A thick fog obscures your vision as you follow the near-invisible path. Despite the fog, you're certain something is watching you.","",0,"","","","","","","",0,0,"","","",0,0,0,0),
-       ("Broken Portal","A lone stone structure lies ahead of you, shattered and broken. The grass around it is burnt, and you find strange objects around it - items not from this world, you''re certain.","",0,"","","","","","","",0,0,"","","",0,0,0,0),
-       ("Bone Field","The stones under your feet turn lighter and lighter, until you realise they're not stones at all. The field ahead of you shines an ivory white, full of bones of various sizes and sources. What brought them here? You'd rather not think about it too much.","",0,"","","","","","","",0,0,"","","",0,0,0,0),
-       ("Wolf","You hear pained whining coming from underneath a fallen tree a wolf has been trapped underneath! It's trying to free itself, but seems to be making no progress.","",0,"","","","","","","",0,0,"","","",0,0,0,0);
+       ("Basement","You are walking downstairs to a cold and damp place with little to no light. It seems to be an old basement.","basement_map.png",0,"","","","","","","",1,4,"sword.png","Sword","Under a dusty blanket there seems to be a Sword. It might come in handy later on... Would you take it?",11,0,1,16),
+       ("Crossroad","As you were moving you realize you''ve come to an intersection in the maze","crossroad_map.png",0,"","","","","","","",0,0,"","","",9,1,8,5),
+       ("Graveyard","In the distance you see some slim silhouettes in the fog. As you walk closer, you realize that those are tombstones and you find yourself in the middle of a graveyard.","graveyard_map.png",1,"","","","","","","",0,0,"","","",0,0,17,1),
+       ("Forest","You find yourself entering a forest","forest_map.png",0,"","","","","","","",0,0,"","","",10,6,3,7),
+       ("Necropolis","You enter an extensive and elaborate burial place of an ancient city","necropolis_map.png",1,"","","","","","","",0,0,"","","",5,12,0,0),
+       ("Fork","This path leads you outside to a fork","fork_map.png",0,"","","","","","","",0,0,"","","",0,0,0,0),
+       ("Treasure Room","You find yourself walking into a treasure room!","treasure_room_map.png",0,"","","","","","","",1,3,"gold.png","Gold","It is your lucky day, you have found some gold...Would you like to take it?",20,0,18,3),
+       ("Cementery","You enter the cemetery causing the undead to become restless","cementery_map.png",1,"","","","","","","",0,0,"","","",22,3,0,0),
+       ("Shed","You find yourself walking in a shed full of feathers!","shed_map.png",0,"","","","","","","",1,2,"feather.png","Feather","You find yourself walking in a shed full of feathers!...Would you take one?",23,5,0,0),
+       ("Bandit Camp","You find yourself in the middle of a bandit camp, they don't look friendly","bandit_camp_map.png",1,"","","","","","","",0,0,"","","",0,2,0,12),
+       ("Pond","You hear a splashing and see a pond up ahead, a mermaid greets you from the water","pond_map.png",1,"","","","","","","",0,0,"","","",6,16,11,13),
+       ("Inn","The smell of food and the sound of laughter greets you as you find yourself at the inn.","inn_map.png",1,"","","","","","","",0,0,"","","",0,0,12,14),
+       ("Shrine","A holy glow radiates from between the trees. A stone shrine is sitting in a clearing in the woods, surrounded by offerings.","shrine_map.png",1,"","","","","","","",1,5,"food.png","Food Offerings","",15,0,13,0),
+       ("Scholar's Lair","A twirling tower looms above you. You see books, tomes and scrolls inside, and a strong arcane energy fills the air","lair_map.png",0,"","","","","","","",1,6,"cloak.png","Invisible Cloak","",0,14,7,0),
+       ("Hut","A worn-down hut stands near the entrance to the forest. An old man is sitting on the porch, carving a wooden figure. From the looks of it, he is making a wooden wolf.","hut_map.png",1,"","","","","","","",0,0,"","","",12,0,2,0),
+       ("Chapel","A sense of dread sits in your stomach as you approach the cold, stone walls of a chapel. The air inside is damp, and the very ground beneath your feet seems to hold its breath.","chapel_map.png",0,"","","","","","","",1,7,"chalice.png","Holy Chalice","",18,0,0,4),
+       ("Cave","You approach an opening in the mountain which is covered in stalactites. It looks like a beast, baring its teeth at you.","cave_map.png",0,"","","","","","","",1,8,"shield.png","Shield","",19,17,0,8),
+       ("Troll Hideout","A putrid smell hits your nose as you find yourself face-to-face with three hulking figures - trolls!","troll_hideout_map.png",1,"","","","","","","",0,0,"","","",0,18,0,20),
+       ("Marsh","Your feet stick to the ground as you make your way through a marsh. A thick fog obscures your vision as you follow the near-invisible path. Despite the fog, you're certain something is watching you.","marsh_map.png",0,"","","","","","","",0,0,"","","",21,8,19,0),
+       ("Broken Portal","A lone stone structure lies ahead of you, shattered and broken. The grass around it is burnt, and you find strange objects around it - items not from this world, you''re certain.","portal_map.png",0,"","","","","","","",1,9,"gemstone.png","Gemstone","",0,20,0,22),
+       ("Bone Field","The stones under your feet turn lighter and lighter, until you realise they're not stones at all. The field ahead of you shines an ivory white, full of bones of various sizes and sources. What brought them here? You'd rather not think about it too much.","bone_field_map.png",0,"","","","","","","",0,0,"","","",0,9,21,23),
+       ("Wolf","You hear pained whining coming from underneath a fallen tree a wolf has been trapped underneath! It's trying to free itself, but seems to be making no progress.","wolf_map.png",0,"","","","","","","",0,0,"","","",0,10,22,0);
 
 insert into current_game(player, current_location, inventory, score)
 values ("", 1, 1, 0)
